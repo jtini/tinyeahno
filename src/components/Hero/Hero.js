ce9160aeb6baff2f5c8c8637562af07b42e30f30
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 16:01:21 -0600

    style little illustrations

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,18 @@
 // @flow
 import React from 'react';
+import heroGraphicBg from './Hero-Graphic-BG.png';
+import heroGraphicBg2x from './Hero-Graphic-BG@2x.png';
+import heroGraphicFg from './Hero-Graphic-FG.svg';
 import './style.scss';
 
 const Hero = () => (
     <section className="hero">
         <h1 className="hero__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
         <h2 className="hero__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
-        <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
+        <div className="hero__graphic-wrapper">
+            <img className="hero__graphic" src={heroGraphicBg} srcSet={`${heroGraphicBg}, ${heroGraphicBg2x} 2x`} width={334} alt="squiggle line" />
+            <img className="hero__graphic" src={heroGraphicFg} width={344} alt="impossible illustration" />
+        </div>
     </section>
 )
 
