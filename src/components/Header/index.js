61e9f7f825a995ebb711c14c113b2b365252203b
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 14:19:15 -0600

    change some wrapper rules

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 // @flow
 import React from 'react'
 import Link from 'gatsby-link'
+import './style.scss'
 
 const Header = () => (
-  <header>
+  <header className="header">
     <Link to="/">Jeremy Tinianow</Link>
-    <a href="mailto:jermtee@gmail.com">jermtee@gmail.com</a>
+    <a href="mailto:jermtee@gmail.com" className="header__link">jermtee@gmail.com</a>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 // @flow
 import React from 'react'
 import Link from 'gatsby-link'
+import './index.scss';
 
 const IndexPage = () => (
   <div>
-    <h1>Hello</h1>
-    <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
+    <section className="index__section">
+      <h1>Hello</h1>
+      <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
+    </section>
   </div>
 )
 
