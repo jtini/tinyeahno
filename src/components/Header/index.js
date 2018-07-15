9438f07d1c3893ec520c1ecf442a5cbd254cba49
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 14:08:20 -0600

    reset link styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,10 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div>
-
-  </div>
+  <header>
+    <Link to="/">Jeremy Tinianow</Link>
+    <a href="mailto:jermtee@gmail.com">jermtee@gmail.com</a>
+  </header>
 )
 
 export default Header
