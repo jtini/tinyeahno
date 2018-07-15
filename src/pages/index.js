792fa984343ff3d8b741aac239d754f516273735
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 14:02:20 -0600

    rip out some more stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hello</h1>
+    <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
   </div>
 )
 
