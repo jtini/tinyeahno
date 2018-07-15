3bf17d6a10ed7a8d0b58a7b308c9d16966e9d0d7
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 15:42:50 -0600

    move hero into hero componnet

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+import './style.scss';
+
+const Hero = () => (
+    <section className="hero">
+        <h1 className="hero__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
+        <h2 className="hero__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
+        <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
+    </section>
+)
+
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 // @flow
 import React from 'react'
 import Link from 'gatsby-link'
+import Hero from '../components/Hero/Hero';
 import './index.scss';
 
 const IndexPage = () => (
   <div>
     <section className="index__section">
-      <h1 className="index__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
-      <h2 className="index__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
-      <p>Barbershop hired an independent contractor to re-design our website, which led to a larger discussion of how we present ourselves to potential clients and partners. Using the website redesign as a jumping-off point, I worked with Barbershop’s leadership to create a digital style guide and resource repository to make it easier for our team to collaborate on internal projects.</p>
+      <Hero />
     </section>
   </div>
 )
