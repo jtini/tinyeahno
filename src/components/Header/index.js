c68cfb5c6a6219a4a9a34d1a65666f36933f13fb
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 13:42:31 -0600

    remove stuff" cl"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,22 +3,8 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link to="/">Gatsby</Link>
-      </h1>
-    </div>
+  <div>
+
   </div>
 )
 
