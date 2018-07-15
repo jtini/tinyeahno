cddc96c6233059f607094964a99a489c45ba1c5d
Jeremy Tinianow <jerms@Jeremys-MacBook-Pro-2.local>
2018-07-15 16:27:56 -0600

    deal with css being a weirdo

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,13 +7,15 @@ import './style.scss';
 
 const Hero = () => (
     <section className="hero">
-        <div className="hero__text">
-            <h1 className="hero__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
-            <h2 className="hero__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
-        </div>
-        <div className="hero__graphic-wrapper">
-            <img className="hero__graphic" src={heroGraphicBg} srcSet={`${heroGraphicBg}, ${heroGraphicBg2x} 2x`} width={334} alt="squiggle line" />
-            <img className="hero__graphic" src={heroGraphicFg} width={344} alt="impossible illustration" />
+        <div className="hero__container">
+            <div className="hero__text">
+                <h1 className="hero__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
+                <h2 className="hero__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
+            </div>
+            <div className="hero__graphic-wrapper">
+                <img className="hero__graphic" src={heroGraphicBg} srcSet={`${heroGraphicBg}, ${heroGraphicBg2x} 2x`} width={334} alt="squiggle line" />
+                <img className="hero__graphic" src={heroGraphicFg} width={344} alt="impossible illustration" />
+            </div>
         </div>
     </section>
 )
