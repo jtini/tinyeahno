4f8af2d9d3e312e9b4f501e2f67e9a19ee381dc3
jtinianow <jermtee@gmail.com>
2018-07-21 13:44:43 -0600

    add design label image

--- src/components/CaseStudyGrid/CaseStudyGrid.js
+++ src/components/CaseStudyGrid/CaseStudyGrid.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import CaseStudyLink from '../CaseStudyLink/CaseStudyLink';
+import DesignImage from './images/Design.png';
 import './style.scss';
 
 type Props = {
@@ -8,8 +9,11 @@ type Props = {
 }
 
 const CaseStudyGrid = (props: Props) => (
-    <section className="case-study-grid">
-        {props.posts.map(edge => <CaseStudyLink key={edge.node.id} post={edge.node} />)}
+    <section className="case-studies">
+        <img className="case-studies__headline" src={DesignImage} alt="Design" width={120} />
+        <div className="case-study-grid">
+            {props.posts.map(edge => <CaseStudyLink key={edge.node.id} post={edge.node} />)}
+        </div>
     </section>
 )
 
