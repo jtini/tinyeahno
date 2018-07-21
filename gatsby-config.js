98c4d79afe888522d3649996124077687b3e6799
jtinianow <jermtee@gmail.com>
2018-07-21 14:31:59 -0600

    add the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,5 +49,26 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: '',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+      },
+    },
   ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,11 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer/Footer'
+
+import favicon32 from '../../static/favicon-32x32.png';
+import favicon16 from '../../static/favicon-16x16.png';
+import maskIcon from '../../static/safari-pinned-tab.svg';
+
 import './style.scss'
 
 const TemplateWrapper = ({ children }: any) => (
@@ -13,6 +18,11 @@ const TemplateWrapper = ({ children }: any) => (
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
+      ]}
+      link={[
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: favicon16 },
+        { rel: 'mask-icon', href: maskIcon, color: '#5bbad5' }
       ]}
     />
     <Header />
