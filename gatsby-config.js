032fc8828f76a17f4937fcfe50db98dddcb6bd22
jtinianow <jermtee@gmail.com>
2018-07-21 16:33:56 -0600

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,5 +70,19 @@ module.exports = {
         display: 'standalone',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-122652934-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+      },
+    },
   ],
 }
