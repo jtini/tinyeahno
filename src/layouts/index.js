15391d31757b0ef49d9126a60601b955f85b94e6
jtinianow <jermtee@gmail.com>
2018-07-21 15:34:29 -0600

    add share image and tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,6 +8,7 @@ import Footer from '../components/Footer/Footer'
 import favicon32 from '../../static/favicon-32x32.png';
 import favicon16 from '../../static/favicon-16x16.png';
 import maskIcon from '../../static/safari-pinned-tab.svg';
+import shareImage from '../../static/shareImage.jpg';
 
 import './style.scss'
 
@@ -16,8 +17,19 @@ const TemplateWrapper = ({ children }: any) => (
     <Helmet
       title="Jeremy Tinianow"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Jeremy Tinianow is a Designer & Maker. Currently, Digital Product Designer at Barbershop in Denver, CO.' },
+        { property: 'og:image', content: shareImage },
+        { property: 'og:image:width', content: 1200 },
+        { property: 'og:image:height', content: 630 },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'Jeremy Tinianow' },
+        { property: 'og:description', content: 'Jeremy Tinianow is a Designer & Maker. Currently, Digital Product Designer at Barbershop in Denver, CO.' },
+        { property: 'og:site_name', content: 'Jeremy Tinianow' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@jtinianow' },
+        { name: 'twitter:creator', content: '@jtinianow' },
+        { name: 'twitter:title', content: 'Jeremy Tinianow' },
+        { name: 'twitter:description', content: 'Jeremy Tinianow is a Designer & Maker. Currently, Digital Product Designer at Barbershop in Denver, CO.' }
       ]}
       link={[
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 },
