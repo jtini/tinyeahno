04ff64a7d315d05f19af15375780cd675371c9d7
jtinianow <jermtee@gmail.com>
2018-07-22 11:32:36 -0600

    adjust subhead sizing and add link in footer

--- src/components/CaseStudyGrid/CaseStudyGrid.js
+++ src/components/CaseStudyGrid/CaseStudyGrid.js
@@ -10,7 +10,7 @@ type Props = {
 
 const CaseStudyGrid = (props: Props) => (
     <section className="case-studies">
-        <img className="case-studies__headline" src={DesignImage} alt="Design" width={120} />
+        <img className="case-studies__headline" src={DesignImage} alt="Design" width={100} />
         <div className="case-study-grid">
             {props.posts.map(edge => <CaseStudyLink key={edge.node.id} post={edge.node} />)}
         </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 // @flow
 import React from 'react';
+import Link from 'gatsby-link'
 import './style.scss';
 
 const Footer = () => (
     <footer className="footer">
-        <p className="footer__text">Jeremy Tinianow</p>
+        <p className="footer__text"><Link to="/">Jeremy Tinianow</Link></p>
         <p className="footer__text">
             <a href="mailto:jermtee@gmail.com" className="footer__link">jermtee@gmail.com</a>
         </p>
