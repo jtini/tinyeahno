dc274ca8264245d38a3aa2019baa921ca02f26c3
jtinianow <jermtee@gmail.com>
2018-08-05 11:46:54 -0600

    add a strikethrough

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => (
         <div className="hero__container">
             <div className="hero__text">
                 <h1 className="hero__headline">Jeremy Tinianow is a Designer&nbsp;&&nbsp;Maker</h1>
-                <h2 className="hero__subheadline h1">Currently, Digital Product Designer at Barbershop in Denver, CO.</h2>
+                <h2 className="hero__subheadline h1">Currently, Digital Product Designer at <span className="strikethrough">Barbershop</span> Spruce Labs in Denver,&nbsp;CO.</h2>
             </div>
             <div className="hero__graphic-wrapper">
                 <img className="hero__graphic" src={heroGraphicBg} srcSet={`${heroGraphicBg}, ${heroGraphicBg2x} 2x`} width={334} alt="squiggle line" />
