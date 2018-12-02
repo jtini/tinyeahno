31350996ed286a87a5f8e47415e6c0d7030da647
jtinianow <jermtee@gmail.com>
2018-12-02 14:47:31 -0700

    case study updates

--- src/templates/csTemplate.js
+++ src/templates/csTemplate.js
@@ -25,34 +25,34 @@ export default function Template({
                 </div>
                 <div className="case-study__summary-wrapper">
                     <h1 className="case-study__headline">{frontmatter.title}</h1>
-                    <div className="case-study__summary">
-                        {frontmatter.client &&
-                            <div className="summary__item">
-                                <p className="summary-item__label">Client</p>
-                                <p className="summary-item__text">{frontmatter.client}</p>
-                            </div>
-                        }
-                        {frontmatter.agency &&
-                            <div className="summary__item">
-                                <p className="summary-item__label">Agency</p>
-                                <p className="summary-item__text">{frontmatter.agency}</p>
-                            </div>
-                        }
-                        {frontmatter.date &&
-                            <div className="summary__item">
-                                <p className="summary-item__label">Year</p>
-                                <p className="summary-item__text">{moment(frontmatter.date).format('YYYY')}</p>
-                            </div>
-                        }
-                        {frontmatter.linkText && frontmatter.linkHref &&
-                            <div className="summary__item">
-                                <p className="summary-item__label">Visit</p>
-                                <p className="summary-item__text">
-                                    <a href={frontmatter.linkHref} target="_blank" rel="noopener noreferrer" className="case-study__text-link">{frontmatter.linkText}</a>
-                                </p>
-                            </div>
-                        }
-                    </div>
+                </div>
+                <div className="case-study__summary">
+                    {frontmatter.client &&
+                        <div className="summary__item">
+                            <p className="summary-item__label">Client</p>
+                            <p className="summary-item__text">{frontmatter.client}</p>
+                        </div>
+                    }
+                    {frontmatter.agency &&
+                        <div className="summary__item">
+                            <p className="summary-item__label">Agency</p>
+                            <p className="summary-item__text">{frontmatter.agency}</p>
+                        </div>
+                    }
+                    {frontmatter.date &&
+                        <div className="summary__item">
+                            <p className="summary-item__label">Year</p>
+                            <p className="summary-item__text">{moment(frontmatter.date).format('YYYY')}</p>
+                        </div>
+                    }
+                    {frontmatter.linkText && frontmatter.linkHref &&
+                        <div className="summary__item">
+                            <p className="summary-item__label">Visit</p>
+                            <p className="summary-item__text">
+                                <a href={frontmatter.linkHref} target="_blank" rel="noopener noreferrer" className="case-study__text-link">{frontmatter.linkText}</a>
+                            </p>
+                        </div>
+                    }
                 </div>
             </section>
             <section
